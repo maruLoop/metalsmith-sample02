7a1e761b8853bd4bda5e20021fbc08a09c9e8fda
Toshiya-Kato <toshiya_kato@dwango.co.jp>
2015-04-30 18:05:32 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+var Metalsmith = require('metalsmith'),
+    markdown   = require('metalsmith-markdown'),
+    templates  = require('metalsmith-templates');
+
+
+Metalsmith(__dirname)
+    .use(markdown())
+    .use(templates('handlebars'))
+    .destination('./build')
+    .build(function(err) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            console.log('Build complete!');
+        }
+    });
